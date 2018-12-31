c00ddb4e76f459917f95cb962789cfc0a2c9b0aa
UlisseMini <ulisse.mini@gmail.com>
2018-12-30 20:55:48 -0500

    added comments

--- client/main.go
+++ client/main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	client := getClient()
-	resp, err := client.Get("https://liz-server:8080")
+	resp, err := client.Get("https://127.0.0.1:8080")
 	mustNot(err)
 
 	defer resp.Body.Close()
@@ -23,26 +23,37 @@ func main() {
 	fmt.Printf("Status: %s  Body: %s\n", resp.Status, string(body))
 }
 
-func getClient() *http.Client {
+func getClient(cert, key string) *http.Client {
 	cp := x509.NewCertPool()
 	data, _ := ioutil.ReadFile("../ca/minica.pem")
 	cp.AppendCertsFromPEM(data)
 
-	// c, _ := tls.LoadX509KeyPair("signed-cert", "key")
+	// If you want to load a client cert
+	// c, err := tls.LoadX509KeyPair("signed-cert", "key")
 
 	config := &tls.Config{
+		// Certificates's is only used when the server is doing client
+		// authentication, normally not because webservers want anyone to
+		// be able to use them
 		// Certificates: []tls.Certificate{c},
-		RootCAs:               cp,
-		GetClientCertificate:  utils.ClientCertReqFunc("cert.pem", "key.pem"),
+
+		// RootCA's are the trusted certificate authories for the client
+		RootCAs: cp,
+
+		// GetClientCertificate allows you to use a custom function
+		// for getting the client certificate (see above)
+		GetClientCertificate: utils.ClientCertReqFunc("cert.pem", "key.pem"),
+
+		// Create a custom function for verifying the peers certificate.
+		// you should avoid this it has been done here for the talk
 		VerifyPeerCertificate: utils.CertificateChains,
 	}
 
-	client := &http.Client{
+	return &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: config,
 		},
 	}
-	return client
 }
 
 func mustNot(err error) {
