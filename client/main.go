0424b2c2e272548e675fc96e92ae50ec29fc8385
UlisseMini <ulisse.mini@gmail.com>
2018-12-30 20:41:13 -0500

    changed 'must' to 'mustNot'

--- client/main.go
+++ client/main.go
@@ -14,11 +14,11 @@ import (
 func main() {
 	client := getClient()
 	resp, err := client.Get("https://liz-server:8080")
-	must(err)
+	mustNot(err)
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	must(err)
+	mustNot(err)
 
 	fmt.Printf("Status: %s  Body: %s\n", resp.Status, string(body))
 }
@@ -45,7 +45,7 @@ func getClient() *http.Client {
 	return client
 }
 
-func must(err error) {
+func mustNot(err error) {
 	if err != nil {
 		fmt.Printf("Client error: %v\n", err)
 		os.Exit(1)
@@ -53,14 +53,14 @@ func must(err error) {
 }
 
 // fmt.Println("Certificate authority:")
-// must(utils.OutputPEMFile("../ca/cert"))
+// mustNot(utils.OutputPEMFile("../ca/cert"))
 // cp, _ := x509.SystemCertPool() or
 // cp := x509.NewCertPool()
 // data, _ := ioutil.ReadFile("../ca/cert")
 // cp.AppendCertsFromPEM(data)
 
 // fmt.Println("My certificate:")
-// must(utils.OutputPEMFile("signed-cert"))
+// mustNot(utils.OutputPEMFile("signed-cert"))
 // c, _ := tls.LoadX509KeyPair("signed-cert", "key")
 
 // InsecureSkipVerify: true,
